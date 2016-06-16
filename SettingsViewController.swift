33e7564812bf8a7e48678f90addc0d0434f713dd
Matt Hills <Matt@matts-macbook-pro.local>
2016-06-15 23:25:56 -0500

    Settings view controller set up.

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
     
     @IBAction func closeViewButtonPressed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: {})
@@ -17,9 +17,22 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //tableView.contentInset = UIEdgeInsetsMake(-35, 0, -35, 0)
+        
         navigationController?.navigationBar.barTintColor = UIColor(red: 0/255, green: 51/255, blue: 0/255, alpha: 1.0)
         navigationController?.navigationBar.barStyle = UIBarStyle.BlackTranslucent
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name:"HelveticaNeue-Light", size: 20)!]
+    }
+}
+
+extension SettingsViewController {
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if (section == 0) {
+            return 30
+        } else {
+            return 12
+        }
     }
 }
