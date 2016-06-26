d6aa84cb58bf6348e05a43d91e2d6e4a1df96dca
Matt Hills <Matt@matts-macbook-pro.local>
2016-06-26 14:48:16 -0500

    Login view controller created and functionality set up.

--- carpoolApp_v1.0/LoginViewController.swift
+++ carpoolApp_v1.0/LoginViewController.swift
@@ -15,8 +15,13 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginButtonTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var loginButtonCenterConstraint: NSLayoutConstraint!
     
     var screenSize = UIScreen.mainScreen().bounds
+    
+    var emailToValidate = String()
+    var passwordToValidate = String()
     
     let userEmail = UserEmail()
     let userPassword = UserPassword()
@@ -35,6 +40,11 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         self.view.addSubview(imageViewBackground)
         self.view.sendSubviewToBack(imageViewBackground)
         
+        emailTextField.attributedPlaceholder = NSAttributedString(string:"Email", attributes: [NSForegroundColorAttributeName: UIColor.darkGrayColor()])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string:"Password", attributes: [NSForegroundColorAttributeName: UIColor.darkGrayColor()])
+        
+        self.loginButtonCenterConstraint.constant = screenSize.width
+        
         emailTextField.tag = 1
         passwordTextField.tag = 2
     }
@@ -42,15 +52,52 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
 
 extension LoginViewController {
     
+    func textFieldDidBeginEditing(textField: UITextField) {
+        loginButtonOutOfView()
+        
+        switch (textField.tag) {
+        case 1:
+            passwordTextField.userInteractionEnabled = false
+        case 2:
+            emailTextField.userInteractionEnabled = true
+        default:
+            break
+        }
+
+    }
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         switch (textField.tag) {
         case 1:
             print("Email return")
+            emailToValidate = emailTextField.text!
+            // TO DO: We'll want to validate the email input here. Search DB for email address matching input. Display alert if no match. Move to password text field if email is found.
+            passwordTextField.userInteractionEnabled = true
+            passwordTextField.becomeFirstResponder()
         case 2:
-            print("Passowrd return")
+            // TO DO: We'll want to validate the password input here. Search DB for password that matches input AND matches email entered prior. Display login button.
+            passwordToValidate = passwordTextField.text!
+            print("Password return")
+            loginButtonInView()
         default:
             break
         }
+        textField.resignFirstResponder()
         return true
+    }
+    
+    func loginButtonInView() {
+        self.loginButtonCenterConstraint.constant = self.screenSize.width
+        UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.75, initialSpringVelocity: 5, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.loginButtonCenterConstraint.constant -= self.screenSize.width
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+    }
+    
+    func loginButtonOutOfView() {
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.loginButtonCenterConstraint.constant = self.screenSize.width
+            self.view.layoutIfNeeded()
+            }, completion: nil)
     }
 }
